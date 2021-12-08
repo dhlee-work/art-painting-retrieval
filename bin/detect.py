1bb8e02806e39109827fe87ba8177510039dabbc
dhlee <dhlee.work@gmail.com>
2021-12-09 02:05:45 +0900

    .

---
+++ bin/detect.py
@@ -0,0 +1,61 @@
+from __future__ import division
+
+import os
+import argparse
+import tqdm
+import random
+import numpy as np
+
+from PIL import Image
+
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+
+from pytorchyolo.models import load_model
+from pytorchyolo.utils.utils import load_classes, rescale_boxes, non_max_suppression, print_environment_info
+from pytorchyolo.utils.datasets import ImageFolder
+from pytorchyolo.utils.transforms import Resize, DEFAULT_TRANSFORMS
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.ticker import NullLocator
+import os
+
+
+def run():
+    print_environment_info()
+    parser = argparse.ArgumentParser(description="Detect objects on images.")
+    parser.add_argument("-m", "--model", type=str, default="config/yolov3-openimages.cfg", help="Path to model definition file (.cfg)")
+    parser.add_argument("-w", "--weights", type=str, default="weights/yolov3-openimages.weights", help="Path to weights or checkpoint file (.weights or .pth)")
+    #parser.add_argument("-i", "--images", type=str, default="data/openimages/picture_frame", help="Path to directory with images to inference")
+    parser.add_argument("-i", "--images", type=str, default="data/old/intelicon_test_od_1102", help="Path to directory with images to inference")
+    parser.add_argument("-c", "--classes", type=str, default="data/openimages.names", help="Path to classes label file (.names)")
+    parser.add_argument("-o", "--output", type=str, default="output_intelicon_test", help="Path to output directory")
+    parser.add_argument("-b", "--batch_size", type=int, default=1, help="Size of each image batch")
+    parser.add_argument("--img_size", type=int, default=416, help="Size of each image dimension for yolo") # 416
+    parser.add_argument("--n_cpu", type=int, default=8, help="Number of cpu threads to use during batch generation")
+    parser.add_argument("--conf_thres", type=float, default=0.1, help="Object confidence threshold")
+    parser.add_argument("--nms_thres", type=float, default=0.4, help="IOU threshold for non-maximum suppression")
+    args = parser.parse_args()
+    print(f"Command line arguments: {args}")
+
+    # Extract class names from file
+    classes = load_classes(args.classes)  # List of class names
+
+    detect_directory(
+        args.model,
+        args.weights,
+        args.images,
+        classes,
+        args.output,
+        batch_size=args.batch_size,
+        img_size=args.img_size,
+        n_cpu=args.n_cpu,
+        conf_thres=args.conf_thres,
+        nms_thres=args.nms_thres)
+
+
+if __name__ == '__main__':
+    run()
---
+++ model/README.md
@@ -0,0 +1,2 @@
+1.openimages dataset yolov3 weights
+2.painting retrieval models weights
